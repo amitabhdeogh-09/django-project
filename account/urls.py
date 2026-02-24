70a7ba4cfdc5fc48a5edf96209d48e2482edc391
amitabhdeogh-09 <amitabh.deogharia.aec@gmail.com>
2026-02-24 12:50:31 +0530

    Initial commit - Django Project

---
+++ account/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from .import views
+
+urlpatterns=[
+     path('registration',views.registerpage,name='registration'),
+     path('login',views.loginpage,name='login'),
+     path('logout',views.logoutpage,name='logout'),
+
+     #after login 
+     path('',views.Home,name='home'),
+     path('product',views.Prod,name='product'),
+     path('customer/<str:pk_test>/',views.Cust,name='customer'),
+     path('customer_list',views.Customer_list,name='customer_list'),
+     path('create_order',views.create_order,name='create_order'),
+     path('place_order/<str:pk>/',views.place_order,name='place_order'),
+     path('update_order/<str:pk>/',views.update_order,name='update_order'),
+     path('updateorder/<str:pk>/',views.updateorder,name='updateorder'),
+     path('delete_order/<str:pk>/',views.deleteorder,name='delete_order'),
+     path('order_list',views.order_list,name='order_list'),
+     path('tag_list',views.tag_list,name='tag_list'),
+     path('import_csv',views.import_tag_csv,name='import_csv'),
+     path('create_customer',views.create_customer,name='create_customer'),
+     path('update_customer/<str:pk>/',views.update_customer,name='update_customer'),
+     path('delete_customer/<str:pk>/',views.deletecustomer,name='delete_customer'),
+     path('add_product',views.add_product,name='add_product'),
+     path('update_product/<str:pk>/',views.update_product,name='update_product'),
+     path('delete_product/<str:pk>/',views.delete_product,name='delete_product'),
+
+     
+     
+
+]
